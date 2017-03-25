080babe536c26c57a5920534b48901a821c61fd7
S-HIBIKINEMOTOM\hibik <nemoto.h.ji1mdv@gmail.com>
2017-03-25 22:09:38 +0900

    first commit

---
+++ CDforD/Assets/GoalArea.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+static var myGoal:boolean;
+myGoal=false;
+function OnTriggerEnter(myCol:Collider)
+    {
+        if(myCol.tag=="Player")
+        {
+            myGoal=true;
+        }
+    }
---
+++ CDforD/Assets/myTimeScript.js
@@ -0,0 +1,35 @@
+// ﻿#pragma strict
+//
+// // function Start () {
+// //
+// // }
+// //
+// // function Update () {
+// //
+// // }
+// static var time:float;
+// function Start () {
+//  time=0;
+// }
+//
+// function Update () {
+//  myGoal();
+// }
+//
+// function myGoal()
+// {
+// if (GoalArea.myGoal==false)
+//  {
+//   time+=Time.deltaTime;
+//  }else{
+// 	 var msg=GameObject.Find("msg");
+//   // var msg=GameObject.Find(”msg”);
+//   // msg.guiText.text=”GOAL!”;
+// 	msg.GetComponent.<GUIText>().text="GOAL!";
+// 	  yield WaitForSeconds (5.0);
+//   Application.LoadLevel(Application.loadedLevel);
+//  }
+//  var now:int=time;
+//  // guiText.text=”<Color=red>TIME:” + now.ToString()+”</Color>”;
+// GetComponent.<GUIText>().text="<Color=red>TIME:"+ now.Tostring()+"</Color>";
+// }
